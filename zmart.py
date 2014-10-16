0b51f16a272eb63dd5cdcc2525f781040d71803e
Gonzalo Muñoz <gonzalo.munoz@elun.cl>
2014-10-16 15:56:39 -0300

    zmart modified

--- zmart.py
+++ zmart.py
@@ -1,15 +1,15 @@
+# -*- encoding: utf-8 -*-
 import requests
 from bs4 import BeautifulSoup
 
 
-## PS4 Games.
-## Only games in stock
+####### PS3 Games : Only in stock ###########
+
 for x in range(1, 15):
 	url = "http://www.zmart.cl/scripts/prodList.asp?idcategory=187&curPage={0}&sortField=price%2C+idproduct&sinstock=0".format(str(x))
 	r = requests.get(url)
 	soup = BeautifulSoup(r.content)
 	#print soup.prettify().encode('UTF-8')
-
 	games = soup.findAll("div", {"class": "caja_minihome"})
 	
 	for game in games:
