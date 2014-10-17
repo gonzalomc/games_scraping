ce6af287ff8f241969e8f291b8600afc71e71d4a
Gonzalo Muñoz <gonzalo@ubican.cl>
2014-10-17 14:03:29 -0300

    Extract xbox 360 games

--- zmart.py
+++ zmart.py
@@ -35,12 +35,45 @@ for x in range(1, 15):
 		game_price =price.text
 
 
-		cur.execute("INSERT INTO games_game (name, price, store_id) VALUES ('{0}', '{1}', 2)".format(game_name, game_price))
+		cur.execute("INSERT INTO games_game (name, price, store_id, console_id) VALUES ('{0}', '{1}', 2, 1)".format(game_name, game_price))
 		
 		print game_name
 		print game_price
 		print '======='
 
+#####################################################
+######### XBOX 360 Games : In Stock #############
+for x in range(1, 15):
+	try:
+		url = "http://www.zmart.cl/scripts/prodList.asp?idcategory=159&curPage={0}&sortField=price%2C+idproduct&sinstock=0".format(str(x))
+		r = requests.get(url)
+		soup = BeautifulSoup(r.content)
+		#print soup.prettify().encode('UTF-8')
+		games = soup.findAll("div", {"class": "caja_minihome"})
+		
+		for game in games:
+			title = game.find("div", {"class": "caja_secundaria"})
+			name = title.find("a")
+			game_data = game.find("ul", {"class": "precio_primero"})
+			price = game_data.find("li", {"class": "precio"})
+			status = game_data.find("li", {"class": "estado"})
+
+			game_name = name.text.encode('UTF-8')
+			game_name = game_name.replace("'", "")
+			game_price =price.text
+
+
+			cur.execute("INSERT INTO games_game (name, price, store_id, console_id) VALUES ('{0}', '{1}', 2, 2)".format(game_name, game_price))
+			
+			print game_name
+			print game_price
+			print '======='
+	except:
+		pass
+
 db.commit()
+
+#####################################################
+
 db.close()
 
