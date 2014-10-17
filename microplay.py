9f645bfeb2036a8c852370edc26f5e398bfcb381
Gonzalo Muñoz <gonzalo@ubican.cl>
2014-10-17 12:17:54 -0300

    Update microplay script

--- microplay.py
+++ microplay.py
@@ -4,23 +4,41 @@ import MySQLdb
 import requests
 from bs4 import BeautifulSoup
 
+####### Mysql Conection #####
+db = MySQLdb.connect(host="localhost",
+	user="root",
+	passwd="root",
+	db="scrp_games")
 
-for x in range(1, 17):
-	try:
-		url = "http://www.microplay.cl/resultados/page:{0}/?buscar=ps3".format(str(x))
-		r = requests.get(url)
-		soup = BeautifulSoup(r.content)
+cur = db.cursor()
+try:
+	cur.execute("DELETE FROM games_game WHERE store_id = 4");
+except:
+	pass
+try:
+	for x in range(1, 17):
+		try:
+			url = "http://www.microplay.cl/resultados/page:{0}/?buscar=ps3".format(str(x))
+			r = requests.get(url)
+			soup = BeautifulSoup(r.content)
 
-		#print soup.prettify().encode('UTF-8')
-		games = soup.findAll("li", {"class": "ps2"})
+			#print soup.prettify().encode('UTF-8')
+			games = soup.findAll("li", {"class": "ps2"})
 
-		for game in games:
-			name_span = game.find("span")
-			name_span = name_span.find("a")
-			game_name = name_span.text
-			game_price =  game.find("strong")
-			print game_name.encode('UTF-8').strip()
-			print game_price.text
-			print '####'
-	except:
-		pass
+			for game in games:
+				name_span = game.find("span")
+				name_span = name_span.find("a")
+				game_name = name_span.text
+				game_price =  game.find("strong")
+				print game_name.encode('UTF-8').strip()
+				print game_price.text
+				print '####'
+				cur.execute("INSERT INTO games_game (name, price, store_id) VALUES ('{0}', '{1}', 4)".format(
+					game_name.encode('UTF-8').strip(), game_price.text))
+		except:
+			pass
+		db.commit()
+except:
+	pass
+
+db.close()
